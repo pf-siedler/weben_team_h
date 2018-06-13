1de91402c953398c8b493fc84895faccdcdbb1c2
Takuma Suzuki <pferd262@gmail.com>
2018-06-13 17:54:55 +0900

    一連の質問の流れを作った

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, abort
+import urllib
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -8,7 +9,9 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, PostbackEvent, TextMessage, TextSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction
+    MessageEvent, PostbackEvent, TextMessage, TextSendMessage,
+    TemplateSendMessage, ButtonsTemplate, CarouselTemplate, CarouselColumn,
+    MessageTemplateAction, URITemplateAction, PostbackTemplateAction
 )
 
 app = Flask(__name__)
@@ -16,7 +19,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ.get("LINE_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get('LINE_CHANNEL_SECRET'))
 
-def askTimeSlot(data):
+def askTimeSlotTemplate(data):
     message = TemplateSendMessage(
         alt_text='時間帯',
         template=ButtonsTemplate(
@@ -43,7 +46,9 @@ def askTimeSlot(data):
 
     return message
 
-def askBudget(data):
+parseQuery = lambda s: urllib.parse.parse_qs(s)
+
+def askBudgetTemplate(data):
     message = TemplateSendMessage(
         alt_text='予算',
         template=ButtonsTemplate(
@@ -70,6 +75,69 @@ def askBudget(data):
 
     return message
 
+def resultsTemplate(data):
+
+    query = parseQuery(data)
+
+    #TODO クエリからデートコース一覧を取得
+    # courses = ...
+
+    place = query["place"][0]
+    timeslot = query["time"][0]
+    budget = query["budget"][0]
+
+    courses = [
+            CarouselColumn(
+                thumbnail_image_url='https://example.com/item1.jpg',
+                title='this is menu1',
+                text='場所{} 時間帯{} 予算{}のイベント一覧'.format(place, timeslot, budget),
+                actions=[
+                    PostbackTemplateAction(
+                        label='postback1',
+                        text='postback text1',
+                        data='action=buy&itemid=1'
+                    ),
+                    MessageTemplateAction(
+                        label='message1',
+                        text='message text1'
+                    ),
+                    URITemplateAction(
+                        label='uri1',
+                        uri='http://example.com/1'
+                    )
+                ]
+            ),
+            CarouselColumn(
+                thumbnail_image_url='https://example.com/item2.jpg',
+                title='this is menu2',
+                text='description2',
+                actions=[
+                    PostbackTemplateAction(
+                        label='postback2',
+                        text='postback text2',
+                        data='action=buy&itemid=2'
+                    ),
+                    MessageTemplateAction(
+                        label='message2',
+                        text='message text2'
+                    ),
+                    URITemplateAction(
+                        label='uri2',
+                        uri='http://example.com/2'
+                    )
+                ]
+            )
+        ]
+
+    message = TemplateSendMessage(
+    alt_text='Carousel template',
+    template=CarouselTemplate(
+        columns=courses
+    ))
+
+    return message
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -87,20 +155,31 @@ def callback():
 
     return 'OK'
 
+places = ["横浜", "東京", "鎌倉"]
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text == "こんにちは":
+    text = event.message.text
+    if text in places:
+
         line_bot_api.reply_message(
         event.reply_token,
-        askBudget("hoge=huga"))
+        askTimeSlotTemplate("place={}".format(text)))
 
 @handler.add(PostbackEvent)
 def handle_postback(event):
+    reply = None
 
-    query = event.postback.data
-    #TODO queryをパース key1=value1&key2=value2... の形式
-
-    reply = askTimeSlot("hoge=huga")
+    data = event.postback.data
+    query =  parseQuery(data)
+    print(query)
+    if query["phase"][0] == "budget":
+        reply = resultsTemplate(data)
+    elif query["phase"][0] == "time":
+        reply = askBudgetTemplate(data)
+    #TODO
+    #else:
+    #    reply =  エラーメッセージを登録
 
     line_bot_api.reply_message(
         event.reply_token,
